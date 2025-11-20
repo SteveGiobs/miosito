85a4b1e32c2d46867289fa0cff331e2eb94b130a
Giovanni Nani <62328644+SteveGiobs@users.noreply.github.com>
2025-11-20 14:20:35 +0100

    Add files via upload

--- calendario.js
+++ calendario.js
@@ -1,6 +1,4 @@
-/* ================================
-   Carico i loghi
-================================ */
+/* ==== CARICO I LOGHI ==== */
 const LOGHI = {
 	savona: "Logo_SavonaBianco.jpg",
 	spezia:"Logo_Spezia.jpg",
@@ -12,6 +10,7 @@ const LOGHI = {
 	amatori:"Logo_Amatori.png"
 };
 
+/* ==== NOMI COMPLETI ==== */
 const nomiSquadre = {
   savona: "Savona",
 	spezia: "RFC Spezia",
@@ -21,97 +20,77 @@ const nomiSquadre = {
 	imperia: "Imperia",
 	cogoleto: "CFFS Vespe Cogoleto",
 	amatori: "Amatori Genova"
-  
 };
 
-/* ================================
-   Elenco Partite
-================================ */
-const partite = [
-  { casa: "Spezia", trasf: "Savona", sc1: 0, sc2: 29, data: "2025-10-19", ora: "15:30" , luogo: "La Spezia"},
-  { casa: "Savona", trasf: "Genova", sc1: 36, sc2: 7, data: "2025-10-26", ora: "14:30" , luogo: "Stadio Fontanassa, Savona"},
-  { casa: "Savona", trasf: "Amatori", sc1: 36, sc2: 17, data: "2025-11-02", ora: "14:30" , luogo: "Stadio Fontanassa, Savona"},
-  { casa: "Province", trasf: "Savona", sc1: null, sc2: null, data: "2025-11-30", ora: "14:30" , luogo: "Campo Carlo Androne, Recco"},
-  { casa: "Savona", trasf: "Recco", sc1: null, sc2: null, data: "2025-12-14", ora: "14:30" , luogo: "Stadio Fontanassa, Savona"},
-  { casa: "Imperia", trasf: "Savona", sc1: null, sc2: null, data: "2026-01-18", ora: "14:30" , luogo: "Imperia"},
-  { casa: "Savona", trasf: "Cogoleto", sc1: null, sc2: null, data: "2026-01-25", ora: "14:30" , luogo: "Stadio Fontanassa, Savona"},
-  { casa: "Savona", trasf: "Spezia", sc1: null, sc2: null, data: "2026-02-01", ora: "14:30" , luogo: "Stadio Fontanassa, Savona"},
-  { casa: "Genova", trasf: "Savona", sc1: null, sc2: null, data: "2026-02-22", ora: "14:30" , luogo: "Stadio Carlini Bollesan, Genova"},
-  { casa: "Amatori", trasf: "Savona", sc1: null, sc2: null, data: "2026-03-01", ora: "14:30" , luogo: "Stadio Carlini Bollesan, Genova"},
-  { casa: "Savona", trasf: "Province", sc1: null, sc2: null, data: "2026-03-15", ora: "14:30" , luogo: "Stadio Fontanassa, Savona"},
-  { casa: "Recco", trasf: "Savona", sc1: null, sc2: null, data: "2026-03-22", ora: "14:30" , luogo: "Campo Carlo Androne, Recco"},
-  { casa: "Savona", trasf: "Imperia", sc1: null, sc2: null, data: "2026-04-12", ora: "15:30" , luogo: "Stadio Fontanassa, Savona"},
-  { casa: "Cogoleto", trasf: "Savona", sc1: null, sc2: null, data: "2026-04-19", ora: "15:30" , luogo: "Cogoleto"}
-];
-
-
-/* ================================
-   GENERAZIONE CARD
-================================ */
+/* ==== GENERAZIONE CARD ==== */
 const container = document.getElementById("matches");
 let nextFound = false;
+fetch('https://SteveGiobs.github.io/miosito/partiteCalendario.json')
+  .then(res => res.json())
+  .then(partiteCalendario => {
+    partiteCalendario.forEach(p => {
+      console.log(p.casa, p.trasf, p.sc1, p.sc2, p.data, p.ora, p.luogo);
 
-partite.forEach(p => {
+      const nonGiocata =
+        p.sc1 === null || p.sc1 === "" || p.sc1 === -1 ||
+        p.sc2 === null || p.sc2 === "" || p.sc2 === -1;
 
-  const nonGiocata =
-    p.sc1 === null || p.sc1 === "" || p.sc1 === -1 ||
-    p.sc2 === null || p.sc2 === "" || p.sc2 === -1;
+      let scoreHTML = nonGiocata ?
+        `<div class='score'>
+          <span class='versus'>VS</span>
+        </div>` :
+        `<div class='score'>
+          <span>${p.sc1}</span>
+            <span class='dash'>–</span>
+          <span>${p.sc2}</span>
+        </div>`;
 
-  let scoreHTML = nonGiocata ?
-  `<div class='score'>
-		<span class='versus'>VS</span>
-	</div>` :
-  `<div class='score'>
-		<span>${p.sc1}</span>
-			<span class='dash'>–</span>
-		<span>${p.sc2}</span>
-	</div>`;
+      const home = p.casa.toLowerCase();
+      const away = p.trasf.toLowerCase();
+      const logoHome = LOGHI[home] || "";
+      const logoAway = LOGHI[away] || "";
+      const nomeHome = nomiSquadre[home];
+      const nomeAway = nomiSquadre[away];
 
-  const home = p.casa.toLowerCase();
-  const away = p.trasf.toLowerCase();
-  const logoHome = LOGHI[home] || "";
-  const logoAway = LOGHI[away] || "";
-  const nomeHome = nomiSquadre[home];
-  const nomeAway = nomiSquadre[away];
+      const resHome = nonGiocata ? "" :
+        (p.sc1 > p.sc2 ? "VINCENTE" : (p.sc1 < p.sc2 ? "PREDENTE" : "PAREGGIO"));
+      const resAway = nonGiocata ? "" :
+        (p.sc2 > p.sc1 ? "VINCENTE" : (p.sc2 < p.sc1 ? "PERDENTE" : "PAREGGIO"));
 
-  const resHome = nonGiocata ? "" :
-    (p.sc1 > p.sc2 ? "VINCENTE" : (p.sc1 < p.sc2 ? "PREDENTE" : "PAREGGIO"));
-  const resAway = nonGiocata ? "" :
-    (p.sc2 > p.sc1 ? "VINCENTE" : (p.sc2 < p.sc1 ? "PERDENTE" : "PAREGGIO"));
+      let badge = "";
+      if (nonGiocata && !nextFound) {
+        badge = `<span class='future-badge'>Prossima partita</span>`;
+        nextFound = true;
+      }
 
-  let badge = "";
-  if (nonGiocata && !nextFound) {
-    badge = `<span class='future-badge'>Prossima partita</span>`;
-    nextFound = true;
-  }
+      container.innerHTML += `
+        <div class="match-card ${nonGiocata ? "future-match" : ""}">
+          <div class="match-main">
+            <div class="team home">
+              <span class="name">${nomeHome.toUpperCase()} </span>
+              <img src="${logoHome}">
+              ${!nonGiocata ? `<span class='result ${resHome}'>${resHome.toUpperCase()}</span>` : ""}
+            </div>
 
-  container.innerHTML += `
-    <div class="match-card ${nonGiocata ? "future-match" : ""}">
-      <div class="match-main">
-        <div class="team home">
-          <span class="name">${nomeHome.toUpperCase()} </span>
-          <img src="${logoHome}">
-          ${!nonGiocata ? `<span class='result ${resHome}'>${resHome.toUpperCase()}</span>` : ""}
+            <div class="center-block">
+              ${badge}
+              ${scoreHTML}
+            </div>
+
+            <div class="team away">
+              <span class="name">${nomeAway.toUpperCase()}</span>
+              <img src="${logoAway}">
+              ${!nonGiocata ? `<span class='result ${resAway}'>${resAway.toUpperCase()}</span>` : ""}
+            </div>
+          </div>
+
+          <div class="match-footer">
+            <div class="item">📅 ${new Date(p.data).toLocaleDateString("it-IT")}</div>
+            <div class="item">🕒 ${p.ora}</div>
+            <div class="item">📍 ${p.luogo}</div>
+          </div>
         </div>
+      `;
+})
 
-       
-		<div class="center-block">
-			${badge}
-			${scoreHTML}
-		</div>
-
-        <div class="team away">
-          <span class="name">${nomeAway.toUpperCase()}</span>
-          <img src="${logoAway}">
-          ${!nonGiocata ? `<span class='result ${resAway}'>${resAway.toUpperCase()}</span>` : ""}
-        </div>
-      </div>
-
-      <div class="match-footer">
-        <div class="item">📅 ${new Date(p.data).toLocaleDateString("it-IT")}</div>
-        <div class="item">🕒 ${p.ora}</div>
-        <div class="item">📍 ${p.luogo}</div>
-      </div>
-    </div>
-  `;
 });
--- classifica.js
+++ classifica.js
@@ -1,6 +1,4 @@
-/* ==========================================================
-   LOGHI SQUADRE
-========================================================== */
+/* ==== CARICO I LOGHI ==== */
 const LOGHI = {
   savona: "Logo_SavonaBianco.jpg",
 	spezia:"Logo_Spezia.jpg",
@@ -12,6 +10,7 @@ const LOGHI = {
 	amatori:"Logo_Amatori.png"
 };
 
+/* ==== NOMI COMPLETI==== */
 const nomiSquadre = {
   savona: "Savona",
 	spezia: "RFC Spezia",
@@ -23,84 +22,6 @@ const nomiSquadre = {
 	amatori: "Amatori Genova"
   
 };
-
-/* ==========================================================
-   PARTITE DEL CAMPIONATO
-========================================================== */
-const partite = [
-  { casa: "Genova", trasf: "Cogoleto", sc1: 17, sc2: 5 , pt1: 4, pt2: 0},
-  { casa: "Imperia", trasf: "Amatori", sc1: 3, sc2: 37 , pt1: 0, pt2: 5},
-  { casa: "Province", trasf: "Recco", sc1: 23, sc2: 15 , pt1: 4, pt2: 0},
-  { casa: "Spezia", trasf: "Savona", sc1: 0, sc2: 29 , pt1: 0, pt2: 5},
-  
-  { casa: "Cogoleto", trasf: "Spezia", sc1: 31, sc2: 20 , pt1: 5, pt2: 0},
-  { casa: "Savona", trasf: "Genova", sc1: 36, sc2: 7 , pt1: 5, pt2: 0},
-  { casa: "Recco", trasf: "Imperia", sc1: 46, sc2: 5 , pt1: 5, pt2: 0},
-  { casa: "Amatori", trasf: "Province", sc1: 3, sc2: 20 , pt1: 0, pt2: 4},
-  
-  { casa: "Recco", trasf: "Cogoleto", sc1: 19, sc2: 10 , pt1: 4, pt2: 0},
-  { casa: "Spezia", trasf: "Imperia", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Savona", trasf: "Amatori", sc1: 36, sc2: 17 , pt1: 5, pt2: 0},
-  { casa: "Genova", trasf: "Province", sc1: 22, sc2: 8 , pt1: 5, pt2: 0},
-  
-  { casa: "Genova", trasf: "Cogoleto", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Imperia", trasf: "Amatori", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Province", trasf: "Recco", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Spezia", trasf: "Savona", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  
-  { casa: "Genova", trasf: "Cogoleto", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Imperia", trasf: "Amatori", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Province", trasf: "Recco", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Spezia", trasf: "Savona", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  
-  { casa: "Genova", trasf: "Cogoleto", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Imperia", trasf: "Amatori", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Province", trasf: "Recco", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Spezia", trasf: "Savona", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  
-  { casa: "Genova", trasf: "Cogoleto", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Imperia", trasf: "Amatori", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Province", trasf: "Recco", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Spezia", trasf: "Savona", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  
-  { casa: "Genova", trasf: "Cogoleto", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Imperia", trasf: "Amatori", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Province", trasf: "Recco", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Spezia", trasf: "Savona", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  
-  { casa: "Genova", trasf: "Cogoleto", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Imperia", trasf: "Amatori", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Province", trasf: "Recco", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Spezia", trasf: "Savona", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  
-  { casa: "Genova", trasf: "Cogoleto", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Imperia", trasf: "Amatori", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Province", trasf: "Recco", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Spezia", trasf: "Savona", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  
-  
-  { casa: "Genova", trasf: "Cogoleto", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Imperia", trasf: "Amatori", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Province", trasf: "Recco", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Spezia", trasf: "Savona", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  
-  { casa: "Genova", trasf: "Cogoleto", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Imperia", trasf: "Amatori", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Province", trasf: "Recco", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Spezia", trasf: "Savona", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  
-  { casa: "Genova", trasf: "Cogoleto", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Imperia", trasf: "Amatori", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Province", trasf: "Recco", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Spezia", trasf: "Savona", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  
-  { casa: "Genova", trasf: "Cogoleto", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Imperia", trasf: "Amatori", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Province", trasf: "Recco", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  { casa: "Spezia", trasf: "Savona", sc1: null, sc2: null , pt1: 0, pt2: 0},
-  
-  
-];
 
 /* ==========================================================
    INIZIALIZZA SQUADRE
@@ -123,75 +44,77 @@ function aggiungiSquadra(nome) {
     };
   }
 }
+fetch('https://SteveGiobs.github.io/miosito/partiteClassifica.json')
+  .then(res => res.json())
+  .then(partiteCalendario => {
+    partiteCalendario.forEach(p => {
+      console.log(p.casa, p.trasf, p.sc1, p.sc2, p.pt1, p.pt2);
 
-/* ==========================================================
-   POPOLA LE SQUADRE DALLE PARTITE
-========================================================== */
-partite.forEach(p => {
-  aggiungiSquadra(p.casa);
-  aggiungiSquadra(p.trasf);
+        partiteCalendario.forEach(p => {
+          aggiungiSquadra(p.casa);
+          aggiungiSquadra(p.trasf);
 
-  // se la partita non è giocata, salta
-  if (p.sc1 === null || p.sc2 === null || p.sc1 === "" || p.sc2 === "") return;
+          // se la partita non è giocata, salta
+          if (p.sc1 === null || p.sc2 === null || p.sc1 === "" || p.sc2 === "") return;
 
-  const home = squadre[p.casa];
-  const away = squadre[p.trasf];
-
-
-  home.giocate++;
-  away.giocate++;
-
-  home.fatti += p.sc1;
-  home.subiti += p.sc2;
-  away.fatti += p.sc2;
-  away.subiti += p.sc1;
-
-  if (p.sc1 > p.sc2) {
-    home.vinte++;
-    away.perse++;
-    home.punti += p.pt1;
-	away.punti += p.pt2;
-  } else if (p.sc1 < p.sc2) {
-    away.vinte++;
-    home.perse++;
-	home.punti += p.pt1;
-    away.punti += p.pt2;
-  } else {
-    home.pareggiate++;
-    away.pareggiate++;
-    home.punti += 2;
-    away.punti += 2;
-  }
-});
-
-/* ==========================================================
-   CALCOLA DIFFERENZE & ORDINA CLASSIFICA
-========================================================== */
-Object.values(squadre).forEach(s => {
-  s.diff = s.fatti - s.subiti;
-});
-
-const classifica = Object.values(squadre)
-  .sort((a, b) => b.punti - a.punti || b.diff - a.diff || b.fatti - a.fatti);
-
-/* ==========================================================
-   GENERA HTML DELLA CLASSIFICA
-========================================================== */
-const tab = document.getElementById("classifica");
-
-classifica.forEach((s, i) => {
+          const home = squadre[p.casa];
+          const away = squadre[p.trasf];
 
 
-  tab.innerHTML += `
-    <div class="classifica-row">
-      <div class="posizione">${i + 1}</div>
-      <div class="logo"><img src="${s.logo}"></div>
-      <div class="squadra">${s.nome.toUpperCase()}</div>
-      <div class="dati">${s.punti} pt</div>
-      <div class="dati extra">${s.vinte}V / ${s.pareggiate}P / ${s.perse}S</div>
+          home.giocate++;
+          away.giocate++;
 
-    </div>
-  `;
-});
+          home.fatti += p.sc1;
+          home.subiti += p.sc2;
+          away.fatti += p.sc2;
+          away.subiti += p.sc1;
 
-    /*  <div class="dati extra">Diff: ${s.diff}</div>*/
+          if (p.sc1 > p.sc2) {
+            home.vinte++;
+            away.perse++;
+            home.punti += p.pt1;
+          away.punti += p.pt2;
+          } else if (p.sc1 < p.sc2) {
+            away.vinte++;
+            home.perse++;
+          home.punti += p.pt1;
+            away.punti += p.pt2;
+          } else {
+            home.pareggiate++;
+            away.pareggiate++;
+            home.punti += 2;
+            away.punti += 2;
+          }
+        });
+
+        /* ==========================================================
+          CALCOLA DIFFERENZE & ORDINA CLASSIFICA
+        ========================================================== */
+        Object.values(squadre).forEach(s => {
+          s.diff = s.fatti - s.subiti;
+        });
+
+        const classifica = Object.values(squadre)
+          .sort((a, b) => b.punti - a.punti || b.diff - a.diff || b.fatti - a.fatti);
+
+        /* ==========================================================
+          GENERA HTML DELLA CLASSIFICA
+        ========================================================== */
+        const tab = document.getElementById("classifica");
+
+        classifica.forEach((s, i) => {
+
+
+          tab.innerHTML += `
+            <div class="classifica-row">
+              <div class="posizione">${i + 1}</div>
+              <div class="logo"><img src="${s.logo}"></div>
+              <div class="squadra">${s.nome.toUpperCase()}</div>
+              <div class="dati">${s.punti} pt</div>
+              <div class="dati extra">${s.vinte}V / ${s.pareggiate}P / ${s.perse}S</div>
+              <div class="dati extra">Diff: ${s.diff}</div>
+            </div>
+          `;
+        })
+    })
+  });
